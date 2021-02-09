ad3efd2eaa014198ff6d3668c6d39c33551a2fbb
fahad-israr <fahad00cms@gmail.com>
2021-02-09 17:08:44 +0530

    Banner Improved

--- cli/src/main/java/dev/fgit/banner.java
+++ cli/src/main/java/dev/fgit/banner.java
@@ -2,7 +2,7 @@ package dev.fgit;
 
 public class banner{
     public static void displayBanner() {
-        System.out.println("______    _____   _____   _______ ");
+        System.out.println(" ______    _____   _____   _______ ");
         System.out.println("|  ____|  / ____| |_   _| |__   __|");
         System.out.println("| |__    | |  __    | |      | |   ");
         System.out.println("|  __|   | | |_ |   | |      | |   ");
