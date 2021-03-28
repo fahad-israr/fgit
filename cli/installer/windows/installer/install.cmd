9ec41d8215bff13ffc88be3301e538120f508493
fahad-israr <fahad00cms@gmail.com>
2021-03-28 19:06:55 +0530

    windows installer fixed

--- cli/installer/windows/installer/install.cmd
+++ cli/installer/windows/installer/install.cmd
@@ -1,7 +1,6 @@
 @echo off
 cd %~dp0
-xcopy /s/i "%~dp0\fgit" "C:\Users\Public\fgit"
+xcopy /s/i "%~dp0\fgit" "%programfiles%\fgit"
 echo Please accept the upcoming Prompts to install successfully
-"%~dp0\fgit.reg" 
-setx path "%path%;C:\Users\Public\fgit"
+setx path "%path%;%programfiles%\fgit"
 PAUSE
